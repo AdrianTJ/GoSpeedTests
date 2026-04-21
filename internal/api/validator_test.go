d7393994b296d5a781ea227363f1c04e164faf4d
Adrian TJ <adrian.tame.jacobo@gmail.com>
2026-04-20 18:02:57 -0600

    Validator test for URLs

---
+++ internal/api/validator_test.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestValidateURL(t *testing.T) {
+	tests := []struct {
+		url     string
+		wantErr bool
+	}{
+		{"https://google.com", false},
+		{"http://example.com/path?query=1", false},
+		{"ftp://example.com", true},           // Invalid scheme
+		{"file:///etc/passwd", true},          // Invalid scheme
+		{"http://localhost", true},            // Loopback
+		{"http://127.0.0.1", true},            // Loopback IP
+		{"http://169.254.169.254", true},      // Link-local
+		{"http://10.0.0.1", true},             // Private range
+		{"http://172.16.0.1", true},           // Private range
+		{"http://192.168.1.1", true},          // Private range
+		{"not-a-url", true},                   // Malformed
+		{"http://[::1]", true},                // IPv6 loopback
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.url, func(t *testing.T) {
+			err := validateURL(tt.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("validateURL(%q) error = %v, wantErr %v", tt.url, err, tt.wantErr)
+			}
+		})
+	}
+}
